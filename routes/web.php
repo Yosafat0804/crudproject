b74b80ed17be9ebedcc55cf1fd6d14bf03e7ffaa
Yosafat <yosafatbagas259@gmail.com>
2025-04-16 16:44:03 +0700

    initial : initialize added crud project

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,4 @@ use Illuminate\Support\Facades\Route;
 // route dengan mode resources
 Route::resource('/products', ProductController::class);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ProductController::class, "index"])->name('home');
